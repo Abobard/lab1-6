1bd90588d42385cff053cc107b3593538cd49b27
Abobard <116433632+Abobard@users.noreply.github.com>
2023-12-27 13:34:39 +0200

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,14 @@
+function changeWhite() {
+    document.getElementById("demo").style.backgroundColor = 'white';
+    document.getElementById("demo").style.color = 'black';
+}
+
+function changeNeutral() {
+    document.getElementById("demo").style.backgroundColor = 'rgb(211, 211, 211)';
+    document.getElementById("demo").style.color = 'white';
+}
+
+function changeBlack() {
+    document.getElementById("demo").style.backgroundColor = 'black';
+    document.getElementById("demo").style.color = 'white';
+}
